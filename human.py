2f5ef4e4669c781eabba50d3af6e8a6f7d8943ed
John Kaushagen <johnkaushagen@gmail.com>
2017-12-17 16:14:34 -0500

    rename some variables

--- human.py
+++ human.py
@@ -1,7 +1,9 @@
-import cv2
-import sys
-import numpy as np
 import cPickle as pickle
+import sys
+
+import cv2
+import numpy as np
+
 from sklearn.svm import SVC
 from sklearn.externals import joblib
 
@@ -18,7 +20,7 @@ def main():
     if sys.argv[1] == 'train':
         train()
     elif sys.argv[1] == 'video':
-        video()
+        do_video()
 
 def frames_to_video(frame_arr, outputpath, fps):
     size = (640, 480)
@@ -30,44 +32,35 @@ def frames_to_video(frame_arr, outputpath, fps):
         out.write(frame)
     out.release()
 
-def plot_boxes(fg, frame):
-    #return fg
-    kernel = np.ones((35, 35), np.uint8)
-    fg = cv2.dilate(fg, kernel, iterations=2)
-    #return fg
+# Unsused atm
+# def plot_boxes(fg, frame):
+#     #return fg
+#     kernel = np.ones((35, 35), np.uint8)
+#     fg = cv2.dilate(fg, kernel, iterations=2)
+#     #return fg
 
-    """mask = np.zeros(fg.shape).astype(np.float32)
-    contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        (x,y,w,h) = cv2.boundingRect(c)
-        if (w > 0) & (h > 0):
-            mask[y:y+h,x:x+w] += 1
-    mx = np.max(mask)
-    if mx > 0:
-        mask /= mx
-    mask = np.uint8(255*mask)
-    #mask = cv2.GaussianBlur(mask,(35,35),0)
-    ret2,mask = cv2.threshold(mask,125,255,cv2.THRESH_OTSU)"""
-    
-    contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
-        if (w > 5) & (h > 5):
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,255), 5)
-    return frame
+#     """mask = np.zeros(fg.shape).astype(np.float32)
+#     contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+#     for c in contours:
+#         (x,y,w,h) = cv2.boundingRect(c)
+#         if (w > 0) & (h > 0):
+#             mask[y:y+h,x:x+w] += 1
+#     mx = np.max(mask)
+#     if mx > 0:
+#         mask /= mx
+#     mask = np.uint8(255*mask)
+#     #mask = cv2.GaussianBlur(mask,(35,35),0)
+#     ret2,mask = cv2.threshold(mask,125,255,cv2.THRESH_OTSU)"""
+
+#     contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+#     for c in contours:
+#         x, y, w, h = cv2.boundingRect(c)
+#         if (w > 5) & (h > 5):
+#             cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,255), 5)
+#     return frame
 
 def train():
-    #print 'Loading dataset...'
-    (X, Y, vX, vY) = inria.load()
-    #print 'Raw dataset shape:',X.shape,Y.shape
-    #print 'Apply feature extractor...'
-    #X = np.array(map(feat,list(X)))
-    #vX = np.array(map(feat,list(vX)))
-    #print 'Done!'
-    #print 'Feature dataset shape:',X.shape,Y.shape
-
-    #for i in range(5):
-    #    inria.disp(str(Y[i]),X[i])
+    X, Y, vX, vY = inria.load()
 
     #Train
     print 'Training model...'
@@ -86,37 +79,30 @@ def train():
     print 'Validation...'
     Y_ = clf.predict(vX)
     v = (Y_ - vY)
-    v[v!=0] = 1
+    v[v != 0] = 1
     print 'Validation accuracy:', (1-np.sum(v)/float(np.size(v)))
 
-def video():
+
+def do_video():
     clf = pickle.load(open(model, 'rb'))
-    #clf = joblib.load(model)
     if cv2.__version__ < '3.0.0':
         fgbg = cv2.BackgroundSubtractorMOG()
     else:
         fgbg = cv2.createBackgroundSubtractorMOG2()
-    vc = cv2.VideoCapture(VIDEO_PATH)
+    video = cv2.VideoCapture(VIDEO_PATH)
 
     fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
     out = cv2.VideoWriter('output.avi', fourcc, 25, FSIZE)
 
-    if vc.isOpened():
-        #fps = vc.get(cv2.cv.CV_CAP_PROP_FPS)
-        #print fps
-
-        # Skip the intro
+    read_success = False
+    if video.isOpened():
         for i in range(260):
-            rval,frame = vc.read()
-
+            read_success, frame = video.read()
         if inria.COL == False:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #fg = fgbg.apply(np.zeros(frame.shape).astype(np.uint8))
-    else:
-        rval = False
     fcount = 1
     #frame_arr = []
-    while rval:
+    while read_success:
         frame = cv2.resize(frame, FSIZE)
         #fg = fgbg.apply(frame)
         fg = fgbg.apply(frame, learningRate=0.01)
@@ -127,9 +113,9 @@ def video():
         fg = cv2.threshold(fg, 125, 255, cv2.THRESH_OTSU)[1]
         mask = fg.copy()
         if cv2.__version__ < '3.0.0':
-            contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         else:
-            __, contours,hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            __, contours, hierarchy = cv2.findContours(fg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         px = []
         loc = []
         for c in contours:
@@ -151,7 +137,7 @@ def video():
         #frame_arr.append(frame)
         out.write(frame)
         #Read new frame
-        rval, frame = vc.read()
+        read_success, frame = video.read()
         if inria.COL == False:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         fcount += 1
@@ -161,7 +147,7 @@ def video():
             break
     #Save frame history
     #frames_to_video(frame_arr,'output.avi',25)
-    vc.release()
+    video.release()
     out.release()
     cv2.destroyAllWindows()
 
